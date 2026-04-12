1713f8f382b7d3d018f9ad3384c8e18011c7a05f
chobocho <chobocho99@gmail.com>
2026-04-12 23:59:35 +0900

    Add 'Quit' functionality to FKeyBar with Alt+F4 key binding

--- src/frontend/src/components/FKeyBar.jsx
+++ src/frontend/src/components/FKeyBar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { Quit } from '../../wailsjs/runtime/runtime'
 import styles from '../styles/FKeyBar.module.css'
 
 export default function FKeyBar({ onCopy, onMove, onNewDir, onDelete, onRename, onHelp, onView, onEdit }) {
+  const handleQuit = () => {
+    try { Quit() } catch { /* 브라우저 개발 환경에서는 무시 */ }
+  }
+
   const keys = [
-    { num: 'F1', label: 'Help',   action: onHelp,   active: !!onHelp },
-    { num: 'F2', label: 'Rename', action: onRename, active: !!onRename },
-    { num: 'F3', label: 'View',   action: onView,   active: !!onView },
-    { num: 'F4', label: 'Edit',   action: onEdit,   active: !!onEdit },
-    { num: 'F5', label: 'Copy',   action: onCopy,   active: !!onCopy },
-    { num: 'F6', label: 'Move',   action: onMove,   active: !!onMove },
-    { num: 'F7', label: 'NewDir', action: onNewDir, active: !!onNewDir },
-    { num: 'F8', label: 'Delete', action: onDelete, active: !!onDelete, danger: true },
-    { num: 'F9', label: 'Menu',   action: null,     active: false },
-    { num: 'F10', label: 'Quit',  action: null,     active: false },
+    { num: 'F1',     label: 'Help',   action: onHelp,   active: !!onHelp },
+    { num: 'F2',     label: 'Rename', action: onRename, active: !!onRename },
+    { num: 'F3',     label: 'View',   action: onView,   active: !!onView },
+    { num: 'F4',     label: 'Edit',   action: onEdit,   active: !!onEdit },
+    { num: 'F5',     label: 'Copy',   action: onCopy,   active: !!onCopy },
+    { num: 'F6',     label: 'Move',   action: onMove,   active: !!onMove },
+    { num: 'F7',     label: 'NewDir', action: onNewDir, active: !!onNewDir },
+    { num: 'F8',     label: 'Delete', action: onDelete, active: !!onDelete, danger: true },
+    { num: 'Alt+F4', label: 'Quit',   action: handleQuit, active: true },
   ]
 
   return (

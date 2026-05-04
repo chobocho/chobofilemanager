82f4351487c1991a1368f347533562d1af635fe6
chobocho <chobocho99@gmail.com>
2026-05-05 00:58:37 +0900

    Bump version to Chobocho Commander V0.2.11

--- src/main.go
+++ src/main.go
@@ -17,7 +17,7 @@ func main() {
 	app := NewApp()
 
 	err := wails.Run(&options.App{
-		Title:            "Chobocho Commander V0.2.4",
+		Title:            "Chobocho Commander V0.2.11",
 		Width:            1280,
 		Height:           800,
 		MinWidth:         900,
